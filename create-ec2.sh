3ae8f6beab7f002b61cd992dc707fb88e702a5c8
vikram Reddy <vikram@gmail.com>
2023-10-12 09:53:35 -0400

    create EC2 instance

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0ce7ba107a8d3fa3a
+DOMAIN_NAME=vikramdannarapu.online
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
 for i in "${NAMES[@]}"
-do
-    echo "Names: $i"
-done
+do  
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
